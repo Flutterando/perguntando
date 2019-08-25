1da74032ddf6b81a87afb0b86ec99df983b7dd5c
Vilson Blanco Dauinheimer <Bwolfnoob@gmail.com>
2019-08-25 04:34:37 -0300

    correcao no like

--- lib/src/home/pages/question/question_bloc.dart
+++ lib/src/home/pages/question/question_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:hasura_connect/hasura_connect.dart';
 import 'package:perguntando/src/repository/hasura_repository.dart';
 import 'package:perguntando/src/shared/models/event/lecture_model.dart';
 import 'package:perguntando/src/shared/models/lecture_question_liked_model.dart';
@@ -11,23 +12,27 @@ class QuestionBloc extends BlocBase {
   final LectureModel lecture;
   final HasuraRepository _hasuraRepository;
   final UserModel user;
-  QuestionBloc(this.tag, this.lecture, this._hasuraRepository, this.user) {}
+  Snapshot<List<LectureQuestionModel>> _getLectures ;
+  QuestionBloc(this.tag, this.lecture, this._hasuraRepository, this.user) {
+    _getLectures = _hasuraRepository.getQuestionLectures(lecture: lecture, user: user);
+  }
 
-  Observable<List<LectureQuestionModel>> get questions => Observable(
-      _hasuraRepository.getQuestionLectures(lecture: lecture, user: user));
+  Observable<List<LectureQuestionModel>> get questions =>
+      Observable(_getLectures.stream);
 
   Future<bool> deleteLectureQuestion(
       LectureQuestionModel lectureQuestion) async {
     return await _hasuraRepository.deleteLectureQuestion(lectureQuestion);
-  }
+  }  
 
   Future<bool> like(LectureQuestionModel lectureQuestion) async {
-    return await _hasuraRepository.createLectureQuestionLiked(lectureQuestion,user.idUser);
+    return await _hasuraRepository.createLectureQuestionLiked(_getLectures,
+        lectureQuestion, user.idUser);
   }
 
   Future<bool> dislike(LectureQuestionModel lectureQuestionLikedModel) async {
-    return await _hasuraRepository
-        .deleteLectureQuestionLiked(lectureQuestionLikedModel,user.idUser);
+    return await _hasuraRepository.deleteLectureQuestionLiked(_getLectures,
+        lectureQuestionLikedModel, user.idUser);
   }
 
   //dispose will be called automatically by closing its streams
